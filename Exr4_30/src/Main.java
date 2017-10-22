0bec85c5f8da12bba0b6155729dd08a7c99579ca
KKhrystyna <khrystynavolosatska@gmaail.com>
2017-10-22 13:32:26 +0300

    Rewritten Exr4_30

--- Exr4_30/src/Main.java
+++ Exr4_30/src/Main.java
@@ -3,59 +3,28 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter 5-digit number:");
-        int number = input.nextInt();
+        boolean repeat = true;
 
-
-        int a = number / 10000;
-        int b = number / 1000 - a * 10;
-        int c = number / 100 - a * 100 - b * 10;
-        int d = number / 10 - a * 1000 - b * 100 - c * 10;
-        int e = number % 10;
-
-        if (a == e && b == d) {
-            System.out.println("Number is palindrome.");
-        }
-        while (a != e || b != d) {
-            System.out.println("Please enter number one more time:");
-            number = input.nextInt();
-            a = number / 10000;
-            b = number / 1000 - a * 10;
-            c = number / 100 - a * 100 - b * 10;
-            d = number / 10 - a * 1000 - b * 100 - c * 10;
-            e = number % 10;
-
-            if (a == e && b == d) {
-                System.out.println("Number is palindrome.");
-            }
-        }
-        if (number > 99999 || number < 10000) {
-            System.out.println("Please enter number one more time:");
-            number = input.nextInt();
-            a = number / 10000;
-            b = number / 1000 - a * 10;
-            c = number / 100 - a * 100 - b * 10;
-            d = number / 10 - a * 1000 - b * 100 - c * 10;
-            e = number % 10;
-
-            if (a == e && b == d) {
-                System.out.println("Number is palindrome.");
-            }
-            while (a != e || b != d) {
-                System.out.println("Please enter number one more time:");
-                number = input.nextInt();
-                a = number / 10000;
-                b = number / 1000 - a * 10;
-                c = number / 100 - a * 100 - b * 10;
-                d = number / 10 - a * 1000 - b * 100 - c * 10;
-                e = number % 10;
-
+        while (repeat) {
+            System.out.println("Please enter 5-digit number:");
+            int number = input.nextInt();
+            if (number < 99999 && number > 10000) {
+                int a = number / 10000;
+                int b = number / 1000 - a * 10;
+                int c = number / 100 - a * 100 - b * 10;
+                int d = number / 10 - a * 1000 - b * 100 - c * 10;
+                int e = number % 10;
                 if (a == e && b == d) {
                     System.out.println("Number is palindrome.");
+                    repeat = false;
+                } else {
+                    System.out.println("NOT palindrome.");
                 }
+
+            } else {
+                System.out.println("Wrong format.");
             }
         }
     }
