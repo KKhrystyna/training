29ac22f71b2f9b368cd93d0f483364398f4d8091
KKhrystyna <khrystynavolosatska@gmaail.com>
2017-10-27 15:49:59 +0300

    Fig5_7

---
+++ Fig5_7/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+        do {
+            System.out.print(a + " ");
+            a++;
+        } while (a <= 10);
+    }
+}
