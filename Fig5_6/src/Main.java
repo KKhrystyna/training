5fb25e9731c5421692b8a382a7b0e4367f21b8f2
KKhrystyna <khrystynavolosatska@gmaail.com>
2017-10-27 15:47:25 +0300

    Book version Fig5_6

--- Fig5_6/src/Main.java
+++ Fig5_6/src/Main.java
@@ -2,15 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int year = 1;
         double amount;
+        double principal = 1000.0;
+        double rate = 0.05;
 
-        System.out.printf("%s %20s %n", "Year", "Amount on deposit");
+        System.out.printf("%s%20s %n", "Year", "Amount on deposit");
 
-        for (double principal = 1000; year <= 10; year++){
-            principal = principal + principal * 5 / 100;
-            amount = principal;
-            System.out.printf("%4d %,20.2f %n", year, amount);
+        for (int year = 1; year <= 10; ++year) {
+            amount = principal * Math.pow(1.0 + rate, year);
+            System.out.printf("%4d%,20.2f%n", year, amount);
         }
     }
 }
